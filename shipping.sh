f320591e4ac000f8b04ed31b1b4ab5c56aa42f3f
sharath1196 <sharat.batt07@gmail.com>
2025-08-11 00:03:44 +0530

    made changes

--- shipping.sh
+++ shipping.sh
@@ -1,66 +1,22 @@
 #!/bin/bash
+app_name="shipping"
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-START_TIME=$(date +%s)
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+source ./common.sh
 
 # check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access" | tee -a $LOG_FILE
-fi
+check_root
 
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+create_user
+
+artifact_setup
 
 dnf install maven -y &>> $LOG_FILE
 VALIDATE $? "Disabling default nodejs"
 
 
-id roboshop
-if [ $? -ne 0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-    VALIDATE $? "Adding a user 'roboshop'"
-else
-    echo "User already created"
-fi
-
-mkdir -p /app
-VALIDATE $? "Making a home directory of roboshop user" 
-
-curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>> $LOG_FILE
-VALIDATE $? "Downloading the artifact"
-
-cd /app 
-rm -rf /app/*
-unzip /tmp/shipping.zip &>> $LOG_FILE
-VALIDATE $? "Extracting the artifact files here"
-
 cd /app 
 mvn clean package &>> $LOG_FILE
-VALIDATE $? "Build the artifact" 
+VALIDATE $? "Building the artifact" 
 
 mv target/shipping-1.0.jar shipping.jar
 VALIDATE $? "Moving the jar file to /app/" 
@@ -95,6 +51,4 @@ fi
 systemctl restart shipping &>>$LOG_FILE
 VALIDATE $? "Restart shipping"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo "Total time taken to execute the script : $TOTAL_TIME seconds" | tee -a $LOG_FILE
+print_time
